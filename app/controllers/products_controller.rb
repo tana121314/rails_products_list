893c06ee8e140342de9a99ce013d22dd4418568e
Tanaka <shinnosuke30532_0326@yahoo.co.jp>
2020-06-11 11:37:15 +0900

    商品登録ページ追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,18 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end  
+
+  def create
+    @product = Product.new(code:params[:code], name:params[:name], stock_number:params[:stock_number], image_url:params[:image_url], note:params[:note])
+    if @product.save
+      redirect_to("/products")
+    else
+      # render 'new'
+      render action: :new
+    end
+  end  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+    validates :code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }, uniqueness: true, length: { maximum: 8 }
+    validates :name, presence: true
+    validates :stock_number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :image_url, length: { minimum: 0 } 
+    validates :note, length: { minimum: 0 } 
 end
+
+
+# 商品コード: 必須、英数字のみ、ユニーク、8文字いない
+# 商品名: 必須
+# 在庫数: 必須、0以上の数値の
+# 商品画像: 任意
+# メモ: 任意
